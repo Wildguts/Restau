a815584352e7393f0910621a245318117e0be6b0
Wildguts <wildmuhamedsghaier@gmail.com>
2025-01-14 06:44:34 +0100

    menu + gallerie

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { GallerieComponent } from './gallerie/gallerie.component';
+import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '', // Default route
+    component: LandingComponent,
+  },
+  {
+    path: 'Gallery',
+    component: GallerieComponent,
+  },
+];
---
+++ src/app/gallerie/gallerie.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gallerie',
+  standalone: true,
+  imports: [],
+  templateUrl: './gallerie.component.html',
+  styleUrl: './gallerie.component.scss'
+})
+export class GallerieComponent {
+
+}
